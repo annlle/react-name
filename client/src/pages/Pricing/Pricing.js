952f945feb33faa35c92e95034b10546d13fd918
annlle <ua.lerus@gmail.com>
2023-01-15 22:48:00 +0200

    Initial commit

---
+++ client/src/pages/Pricing/Pricing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
+const Pricing = () => {
+    return (
+        <>
+            <Header />
+            <div style={{margin: '50px', fontWeight: 'bold', fontSize: '72px'}}>
+                Pricing page
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default Pricing;
